943fe4fde86f4b1a167011468fb0a223776a82d9
Tom Schneider <tomschneider@Toms-MacBook-Pro.local>
2020-02-05 14:18:26 -0500

    Updated API call to test

--- src/app/components/LeagueTracker.vue
+++ src/app/components/LeagueTracker.vue
@@ -1,38 +1,36 @@
 <template>
-    <div class="tracker-wrapper">
+    <div>
       <input type="text" name="summonerID" id="summonerInputId">
       <button name="submit" id="submitButton"></button>
     </div>
 </template>
 <script>
 import 'utils/http.js'
-
 export default {
   name: "LeagueTracker",
   props: {
     query: String
   },
+  methods: {
+    async apiCall() {
+      const request = new XMLHttpRequest()
+  
+      request.open('GET', 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Thorin087?api_key=RGAPI-77652b70-d6da-4a63-b573-f878f2006ef6', true),
+      
+      request.onload = function () {
+        var data = JSON.parse(this.response)
+        // eslint-disable-next-line no-console
+        console.log(data)
+      }
+      request.send();
+    }
+  },
   mounted() {
     // console.log(RIOT_URL,GET_SUMMONER,document.querySelector('#summonerInputId'),API_KEY)
-    // document.querySelector('#submitButton').addEventListener('click', () => {
-      // fetch('/api/users').then((response) => {
-      fetch('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Thorin087?api_key=RGAPI-77652b70-d6da-4a63-b573-f878f2006ef6').then((response) => {
-        // eslint-disable-next-line no-console
-        console.log(response)
-        return response.json();
-      })
-      .then((myJson) => {
-        // eslint-disable-next-line no-console
-        console.log(myJson);
-      });
-
-      //   // eslint-disable-next-line no-console
-      
-  }
+    this.apiCall();
+  },
 }
 </script>
 <style lang="scss" scoped>
-  .tracker-wrapper {
-    position: absolute;
-  }
+
 </style>
