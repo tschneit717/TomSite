57c779d628c593310a5fbca4c746efaee69d5b96
Tom Schneider <tschneit717@gmail.com>
2020-02-20 19:56:09 -0500

    One day these commits will be more organized

--- src/app/components/canvas.vue
+++ src/app/components/canvas.vue
@@ -1,9 +1,26 @@
 <template>
-  
+  <div>
+
+  </div>
 </template>
 <script>
   export default {
-    
+    name:'Timeline',
+    props: {
+      scale: Number,
+      color: {
+        type: String,
+        default: 'default'
+      },
+      effects: {
+        type: Boolean,
+        default: true,
+      }
+    }
+  }
+  if (this.effects) {
+    // eslint-disable-next-line no-console
+    console.log('success')  
   }
 </script>
 <style lang="scss" scoped>
