c93ae1e9a77c841ba8b98df7b456238a3b94176a
Tom Schneider <tschneit717@gmail.com>
2020-02-26 13:39:04 -0500

    updated the timeline position calculation to check if the year is duplicated. If the year is duplicated, then multiply the number of times by the number of duplicates

--- src/app/components/canvas.vue
+++ src/app/components/canvas.vue
@@ -41,27 +41,44 @@
         return timelinePosition;
 
       },
+      checkDuplicateYear(items) {
+        items.forEach(event => {
+          event
+        })
+      },
       init(){
         const currentYear = new Date().getFullYear();
         const startYear = Number(this.items[0].year)
         const timelineLength = currentYear - startYear;
-        const timelineScale = 10000;
+        const timelineScale = (window.innerWidth * 5);
         
+        console.log(this.items.length)
         const timelineItemSize = timelineScale / timelineLength;
 
         const timelineItem = document.querySelectorAll('.timeline-element')
 
         let count = 0;
+        let dupYearCount = 0;
         timelineItem.forEach(item => {
           const itemPosition = this.getPosition(this.items[count].year, startYear, timelineItemSize)
           let currCount = this.items[count].year;
           if (count > 0) {
             let prevCount = this.items[count-1].year
-             if (currCount == prevCount) {
-              item.style.left = itemPosition + timelineItemSize + 'px';
+            console.log(currCount, prevCount)
+             if (currCount === prevCount) {
+               dupYearCount++;
+              if (dupYearCount > 0) {
+                item.style.left = `${itemPosition + (dupYearCount * timelineItemSize)}px`;
+              } else {
+                item.style.left = itemPosition + timelineItemSize + 'px';
+              }
             }
             else {
-              item.style.left = itemPosition + 'px';
+              if (dupYearCount > 0) {
+                item.style.left = `${itemPosition + (dupYearCount * timelineItemSize)}px`;
+              } else {
+                item.style.left = itemPosition + 'px';
+              }
             }
           }
           item.style.width = timelineItemSize + 'px';
@@ -79,12 +96,12 @@
 <style lang="scss" scoped>
   .timeline{
     &-wrapper {
-      max-width: 100vw;
       height:100vh;
+      max-width:100vw;
       overflow: auto;
       display: flex;
       align-items: center;
-      justify-content: center;
+      justify-content: baseline;
     }
     &-element {
       display: block;
@@ -99,9 +116,8 @@
   }
   ul {
     display: flex;
-    width:100%;
     li {
-      position: absolute;
+      position: relative;
       list-style: none;
     }
   }
