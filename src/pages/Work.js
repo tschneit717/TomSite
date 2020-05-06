5a912cfcdbf76c1022ab0cefd2b08b8e8aa11080
Thomas Schneider <tschneit717@gmail.com>
2020-05-06 16:45:44 -0400

    further updates

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-
+import resume from './../assets/data/resume.json'
+import styles from './../assets/styles/_Timeline.module.scss'
 class WorkPage extends Component {
   render() {
+    console.log(resume[0].name)
     return(
       <div className='body-wrapper'>
         <h2>
           This is the Work page
         </h2>
+        <ul className={styles.timeline}>
+          {Object.values(resume).map((item) => {
+            return(
+              <li key={item.date}>{item.name}</li>
+            )                
+          })}
+        </ul>
       </div>
     )
   }
